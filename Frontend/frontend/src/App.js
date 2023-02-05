b7e1225f53d42664adea69a02a6a5831b85f5c10
Ojas Aklecha <90605717+ojasaklechayt@users.noreply.github.com>
2023-02-05 17:49:00 +0530

    Add files via upload

---
+++ Frontend/frontend/src/App.js
@@ -0,0 +1,45 @@
+import Header from "./components/Header";
+import Blogs from "./components/Blogs";
+import UserBlogs from "./components/UserBlogs";
+import BlogDetail from "./components/BlogDetail";
+import AddBlog from "./components/AddBlog";
+
+import React, { useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import Auth from "./components/Auth";
+import { useDispatch, useSelector } from "react-redux";
+import { authActions } from "./store";
+function App() {
+  const dispath = useDispatch();
+
+  const isLoggedIn = useSelector((state) => state.isLoggedIn);
+  console.log(isLoggedIn);
+  useEffect(() => {
+    if (localStorage.getItem("userId")) {
+      dispath(authActions.login());
+    }
+  }, [dispath]);
+  return (
+    <React.Fragment>
+      <header>
+        <Header />
+      </header>
+      <main>
+        <Routes>
+          {!isLoggedIn ? (
+            <Route path="/auth" element={<Auth />} />
+          ) : (
+            <>
+              <Route path="/blogs" element={<Blogs />} />
+              <Route path="/blogs/add" element={<AddBlog />} />
+              <Route path="/myBlogs" element={<UserBlogs />} />
+              <Route path="/myBlogs/:id" element={<BlogDetail />} />{" "}
+            </>
+          )}
+        </Routes>
+      </main>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ Frontend/frontend/src/components/Header.js
@@ -0,0 +1,103 @@
+import React, { useState } from "react";
+import {
+  AppBar,
+  Typography,
+  Toolbar,
+  Box,
+  Button,
+  Tabs,
+  Tab,
+} from "@mui/material";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { authActions } from "../store";
+import { useStyles } from "./utils";
+const Header = () => {
+  const classes = useStyles();
+  const dispath = useDispatch();
+  const isLoggedIn = useSelector((state) => state.isLoggedIn);
+
+  const [value, setValue] = useState();
+  return (
+    <AppBar
+      position="sticky"
+      sx={{
+        background:
+          "linear-gradient(90deg, rgba(238,98,12,0.9417016806722689) 0%, rgba(253,145,29,0.8996848739495799) 50%, rgba(252,238,69,0.9136904761904762) 100%)",
+      }}
+    >
+      <Toolbar>
+        <Typography className={classes.font} variant="h4">
+          Ojas's Blogs
+        </Typography>
+        {isLoggedIn && (
+          <Box display="flex" marginLeft={"auto"} marginRight="auto">
+            <Tabs
+              textColor="inherit"
+              value={value}
+              onChange={(e, val) => setValue(val)}
+            >
+              <Tab
+                className={classes.font}
+                LinkComponent={Link}
+                to="/blogs"
+                label="All Blogs"
+              />
+              <Tab
+                className={classes.font}
+                LinkComponent={Link}
+                to="/myBlogs"
+                label="My Blogs"
+              />
+              <Tab
+                className={classes.font}
+                LinkComponent={Link}
+                to="/blogs/add"
+                label="Add Blog"
+              />
+            </Tabs>
+          </Box>
+        )}
+        <Box display="flex" marginLeft="auto">
+          {!isLoggedIn && (
+            <>
+              {" "}
+              <Button
+                LinkComponent={Link}
+                to="/auth"
+                variant="contained"
+                sx={{ margin: 1, borderRadius: 10 }}
+                color="warning"
+              >
+                Login
+              </Button>
+              <Button
+                LinkComponent={Link}
+                to="/auth"
+                variant="contained"
+                sx={{ margin: 1, borderRadius: 10 }}
+                color="warning"
+              >
+                Signup
+              </Button>
+            </>
+          )}
+          {isLoggedIn && (
+            <Button
+              onClick={() => dispath(authActions.logout())}
+              LinkComponent={Link}
+              to="/auth"
+              variant="contained"
+              sx={{ margin: 1, borderRadius: 10 }}
+              color="warning"
+            >
+              Logout
+            </Button>
+          )}
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
