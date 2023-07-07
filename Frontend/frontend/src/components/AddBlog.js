a364a8325626362e0f43607f2dfff14e83725984
Ojas Aklecha <90605717+ojasaklechayt@users.noreply.github.com>
2023-07-08 01:48:59 +0530

    Update AddBlog.js

--- Frontend/frontend/src/components/AddBlog.js
+++ Frontend/frontend/src/components/AddBlog.js
@@ -21,7 +21,7 @@ const AddBlog = () => {
   };
   const sendRequest = async () => {
     const res = await axios
-      .post("http://localhost:5000/api/blog/add", {
+      .post("http://oju-s-blogs.vercel.app/api/blog/add", {
         title: inputs.title,
         description: inputs.description,
         image: inputs.imageURL,
