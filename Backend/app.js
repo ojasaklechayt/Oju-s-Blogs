b61b2ab9b9cb2c1bab1da87d6ca850cf2462ae56
Ojas Aklecha <90605717+ojasaklechayt@users.noreply.github.com>
2023-07-08 02:22:40 +0530

    Add files via upload

--- Backend/app.js
+++ Backend/app.js
@@ -6,19 +6,41 @@ import cors from "cors";
 import dotenv from "dotenv";
 
 dotenv.config();
+
 const app = express();
 
 app.use(cors({
   origin: 'https://oju-blogs.vercel.app'
 }));
+
 app.use(express.json());
+
 app.use("/api/user", router);
 app.use("/api/blog", blogRouter);
 
-mongoose
-  .connect(process.env.MONGO_URL)
-  .then(() => app.listen(5000))
-  .then(() =>
-    console.log("Connected to Database and Listening to Localhost 5000")
-  )
-  .catch((err) => console.log(err));
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  app.listen(5000, () => {
+    console.log("Connected to Database and Listening to Localhost 5000");
+  });
+})
+.catch((err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+app.use((req, res) => {
+  res.status(404).json({
+    message: "404 Not Found"
+  });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({
+    message: "Internal Server Error"
+  });
+});
