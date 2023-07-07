26750f885d13145fd8358ef4dc0ebeede218f9ee
Ojas Aklecha <90605717+ojasaklechayt@users.noreply.github.com>
2023-07-08 01:50:23 +0530

    Update Blogs.js

--- Frontend/frontend/src/components/Blogs.js
+++ Frontend/frontend/src/components/Blogs.js
@@ -6,7 +6,7 @@ const Blogs = () => {
   const [blogs, setBlogs] = useState();
   const sendRequest = async () => {
     const res = await axios
-      .get("http://localhost:5000/api/blog")
+      .get("https://oju-s-blogs.vercel.app/api/blog")
       .catch((err) => console.log(err));
     const data = await res.data;
     return data;
