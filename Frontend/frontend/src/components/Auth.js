5629dd73916710a4d13137480f8b1e9a4ca94d0c
Ojas Aklecha <90605717+ojasaklechayt@users.noreply.github.com>
2023-07-08 01:49:25 +0530

    Update Auth.js

--- Frontend/frontend/src/components/Auth.js
+++ Frontend/frontend/src/components/Auth.js
@@ -22,7 +22,7 @@ const Auth = () => {
   };
   const sendRequest = async (type = "login") => {
     const res = await axios
-      .post(`http://localhost:5000/api/user/${type}`, {
+      .post(`https://oju-s-blogs.vercel.app/api/user/${type}`, {
         name: inputs.name,
         email: inputs.email,
         password: inputs.password,
