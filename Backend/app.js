c8beeaf1bca1cea69c3f9b935c77290e8d94c2ce
Ojas Aklecha <90605717+ojasaklechayt@users.noreply.github.com>
2023-07-10 12:37:11 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -14,7 +14,7 @@ app.use("/api/user", router);
 app.use("/api/blog", blogRouter);
 
 mongoose
-  .connect(mongodb+srv://blogadmin:blogadmin@cluster0.qgghli6.mongodb.net/Blog?retryWrites=true&w=majority, {
+  .connect("mongodb+srv://blogadmin:blogadmin@cluster0.qgghli6.mongodb.net/Blog?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
