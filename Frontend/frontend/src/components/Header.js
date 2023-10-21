56928a04faa80eb876a18f6a0594e56bceb4b8b2
Harrmit Chauhan <harmitchauhan1@gmail.com>
2023-10-21 19:20:39 -0400

    improved HomePage UI

--- Frontend/frontend/src/components/Header.js
+++ Frontend/frontend/src/components/Header.js
@@ -23,15 +23,17 @@ const Header = () => {
       position="sticky"
       sx={{
         background:
-          "linear-gradient(90deg, rgba(238,98,12,0.9417016806722689) 0%, rgba(253,145,29,0.8996848739495799) 50%, rgba(252,238,69,0.9136904761904762) 100%)",
-      }}
-    >
+          "linear-gradient(90deg, rgba(242, 230, 197, 1) 0%, rgba(157, 208, 255, 1) 100%)",
+        fontFamily:
+          "Roboto"
+      }}>
+
       <Toolbar>
-        <Typography sx={{fontFamily:"monospace"}} variant="h5">
+        <Typography sx={{fontFamily:"Roboto", color: "#000", marginLeft: "30px"}} variant="h5">
           Oju's Blogs
         </Typography>
         {isLoggedIn && (
-          <Box display="flex" marginLeft={"auto"} marginRight="auto">
+          <Box display="flex" marginLeft={"auto"} marginRight={"auto"}>
             <Tabs
               textColor="inherit"
               value={value}
@@ -42,52 +44,56 @@ const Header = () => {
                 LinkComponent={Link}
                 to="/blogs"
                 label="All Blogs"
+                style={{color: '#000', textTransform: 'none', fontSize: '18px', fontWeight: 'bold'}}
               />
               <Tab
                 className={classes.font}
                 LinkComponent={Link}
                 to="/myBlogs"
                 label="My Blogs"
+                style={{color: '#000', textTransform: 'none', fontSize: '18px', fontWeight: 'bold'}}
               />
               <Tab
                 className={classes.font}
                 LinkComponent={Link}
                 to="/blogs/add"
                 label="Add Blog"
+                style={{color: '#000', textTransform: 'none', fontSize: '18px', fontWeight: 'bold'}}
               />
             </Tabs>
           </Box>
         )}
-        <Box display="flex" marginLeft="auto">
-          {!isLoggedIn && (
-            <>
-              {" "}
+          <Box display="flex" marginLeft="auto">
+            {!isLoggedIn && (
+              <>
+                {" "}
+                <Button
+                  LinkComponent={Link}
+                  to="/auth"
+                  variant="contained"
+                  sx={{ margin: 1, borderRadius: 10 }}
+                  color="warning"
+                >
+                  Login / Signup
+                </Button>
+              </>
+            )}
+            {isLoggedIn && (
               <Button
+                onClick={() => dispath(authActions.logout())}
                 LinkComponent={Link}
                 to="/auth"
                 variant="contained"
                 sx={{ margin: 1, borderRadius: 10 }}
-                color="warning"
+                color="primary"
               >
-                Login / Signup
+                Logout
               </Button>
-            </>
-          )}
-          {isLoggedIn && (
-            <Button
-              onClick={() => dispath(authActions.logout())}
-              LinkComponent={Link}
-              to="/auth"
-              variant="contained"
-              sx={{ margin: 1, borderRadius: 10 }}
-              color="warning"
-            >
-              Logout
-            </Button>
-          )}
-        </Box>
+            )}
+          </Box>
       </Toolbar>
     </AppBar>
+
   );
 };
 
--- Frontend/frontend/src/components/LandingPage.js
+++ Frontend/frontend/src/components/LandingPage.js
@@ -1,15 +1,30 @@
 import { color } from "@mui/system";
 import React from "react";
 import { ReactComponent as YourSvg } from '../media/1534214288.svg';
+import { ReactComponent as YourFirstSvg } from '../media/Focus-rafiki.svg';
+
 const LandingPage = () => {
     return <div style={{}}>
-        <div style={{background: "linear-gradient(to bottom right, #ffcc66 38%, #ff6666 90%)",height:"730px"}}>
-            <div style={{display:"flex",flexDirection:"row",justifyContent:"center",alignItems:"center", gap:"5%"}}>
-                <h1 style={{fontSize:"4rem", fontFamily:"monospace"}}>Welcome to my blog</h1>
-                <YourSvg width="300px" height="600px"/>
-            </div>
-        </div>
-    </div>;
+
+    <div style={{ background: "linear-gradient(to bottom right, #f2e6c5, #9dd0ff)", height: "610px", display: "flex" }}>
+
+        <div>
+            <h1 style={{ fontSize: "6rem", fontFamily: "Roboto", textAlign: "left", marginLeft: "100px", marginTop: "50px" }}>
+                <div style={{ display: "inline-block" }}>
+                    <div>Welcome</div>
+                    <div>to my</div>
+                    <div style={{fontSize: "12rem"}}>Blog</div>
+                </div>
+            </h1>
+        </div>  
+        <div style={{  float: "right", display: "flex"}}>
+                <div style={{  marginLeft: "180px" }}>
+                    <YourFirstSvg width="620px" height="595px"/>
+                </div>
+        </div>           
+    </div>
+</div>
+
 };
 
 export default LandingPage;
