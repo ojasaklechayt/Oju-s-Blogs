3ab90ffbca556f581c5a4635ff2c761b6602fb6e
pooja <poojalohar789@gmail.com>
2023-10-21 21:28:53 +0530

    add error handling

--- Frontend/frontend/src/components/Auth.js
+++ Frontend/frontend/src/components/Auth.js
@@ -13,13 +13,20 @@ const Auth = () => {
     email: "",
     password: "",
   });
+  const mailReg = /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/;
+  const nameReg = /^[A-Za-z\s'-]+$/;
+  const allspace = /^\s*$/;
+  const passReg = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@#$%^&+=*!])[A-Za-z\d@#$%^&+=*!]{8,}$/;
+
   const [isSignup, setIsSignup] = useState(false);
+
   const handleChange = (e) => {
     setInputs((prevState) => ({
       ...prevState,
       [e.target.name]: e.target.value,
     }));
   };
+  
   const sendRequest = async (type = "login") => {
     const res = await axios
       .post(`https://oju-blog-backend.onrender.com/api/user/${type}`, {
@@ -34,23 +41,56 @@ const Auth = () => {
     return data;
   };
 
+  const handleErrors = async ()=>{
+    var error = {
+      nameError : false,
+      passError : false,
+      mailError : false
+    }
+    error.nameError = isSignup && (inputs.name.match(allspace) || !inputs.name.match(nameReg))
+    error.mailError = !inputs.email.match(mailReg)
+    error.passError = !inputs.password.match(passReg)
+
+    return error;
+  }
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(inputs);
-    if (isSignup) {
-      sendRequest("signup")
-        .then((data) => localStorage.setItem("userId", data.user._id))
-        .then(() => dispath(authActions.login()))
-        .then(() => naviagte("/blogs"));
-    } else {
-      sendRequest()
-        .then((data) => localStorage.setItem("userId", data.user._id))
-        .then(() => dispath(authActions.login()))
-        .then(() => naviagte("/blogs"));
-    }
+    //validating inputs before sending any request
+    handleErrors()
+    .then((error)=>{
+      if(error.nameError){
+        alert("Kindly give valid name")
+      }
+      if(error.mailError){
+        alert("Kindly give valid email")
+      }
+      if(error.passError){
+        alert("Kindly enter strong password")
+      }
+      else{
+        if (isSignup) {
+          sendRequest("signup")
+          .then((data) => localStorage.setItem("userId", data.user._id))
+          .then(() => dispath(authActions.login()))
+          .then(() => naviagte("/blogs"))
+          .catch(()=>{
+            alert("Sorry something went wrong during signup.")
+          })
+        } else {
+          sendRequest()
+          .then((data) => localStorage.setItem("userId", data.user._id))
+          .then(() => dispath(authActions.login()))
+          .then(() => naviagte("/blogs"))
+          .catch(()=>{
+            alert("Something went wrong. Enter valid login credentials")
+          })
+        }
+      }
+    })
   };
-  return (
-    <div>
+    return (
+      <div>
       <form onSubmit={handleSubmit}>
         <Box
           maxWidth={400}
@@ -63,19 +103,19 @@ const Auth = () => {
           margin="auto"
           marginTop={5}
           borderRadius={5}
-        >
+          >
           <Typography variant="h2" padding={3} textAlign="center">
             {isSignup ? "Signup" : "Login"}
           </Typography>
           {isSignup && (
             <TextField
-              name="name"
-              onChange={handleChange}
-              value={inputs.name}
-              placeholder="Name"
-              margin="normal"
+            name="name"
+            onChange={handleChange}
+            value={inputs.name}
+            placeholder="Name"
+            margin="normal"
             />
-          )}{" "}
+            )}{" "}
           <TextField
             name="email"
             onChange={handleChange}
@@ -83,7 +123,15 @@ const Auth = () => {
             type={"email"}
             placeholder="Email"
             margin="normal"
-          />
+            />
+            {
+              isSignup && 
+              <p>
+                Password must be atleast 8 characters <br/>
+                atleast one lower case, one uppercase <br/>
+                atleast one digit, one special character 
+              </p>
+            }
           <TextField
             name="password"
             onChange={handleChange}
