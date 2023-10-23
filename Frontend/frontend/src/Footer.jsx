98d5000b748e2d130dffee97f12f1adebe20e476
Ojas Aklecha <90605717+ojasaklechayt@users.noreply.github.com>
2023-10-23 09:40:52 +0530

    Update Footer.jsx

--- Frontend/frontend/src/Footer.jsx
+++ Frontend/frontend/src/Footer.jsx
@@ -8,9 +8,9 @@ const Footer = () => {
             <div className="developedby">
 
               <a href="https://github.com/ojasaklechayt">
-              Developed in Open Source by <span className='colorchange'>Ojas Aklecha </span></a>and
+              Developed in Open Source by <span className='colorchange'>Ojas Aklecha </span></a>
 
-<a href="https://github.com/ojasaklechayt/Oju-s-Blogs"><span className='colorchange'> Community</span></a>
+<a href="https://github.com/ojasaklechayt/Oju-s-Blogs">and<span className='colorchange'> Community</span></a>
             </div>
         </footer>
     </>
